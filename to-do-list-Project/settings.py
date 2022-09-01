edaa15d688a7335554f3db26369098f89c3e2b50
Roman <coderc770@gmail.com>
2022-09-01 19:33:22 +0300

    connenction to Heroku

--- to-do-list-Project/settings.py
+++ to-do-list-Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1","todo-list-2022-app.herokuapp.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "todo-list-2022-app.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
